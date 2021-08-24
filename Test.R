2546ec15d5b275ab933843bb00b1bcf514d0d66f
venkat <rkvenkat2@gmail.com>
2021-08-24 16:27:44 +0530

    Added extra code

--- Test.R
+++ Test.R
@@ -7,45 +7,61 @@ library(rgeos)
 library(patchwork)
 library(showtext)
 
+library(tidytuesdayR)
+
+chain_investment <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-08-10/chain_investment.csv')
+
+tidytuesdayR::tt_load("2021-08-10")
 
 
 
-font_add_google("Lobster", "Lobster")
-font_add_google("Montserrat", "Montserrat")
-font_add_google("Roboto Mono", "Roboto Mono")
+head(chain_investment)
+View(chain_investment)
 
-theme_set(theme_bw(base_family = "Lobster"))
-theme_update(rect = element_rect(fill = "#e8d8c3",
-                                 color = "#e8d8c3"),
-             panel.grid.major = element_blank(),
-             panel.grid.minor = element_blank(),
-             panel.background = element_rect(fill = "#e8d8c3",
-                                             color = NA),
-             panel.border = element_blank(),
-             plot.background = element_rect(fill = "#e8d8c3",
-                                            color = "#e8d8c3"),
-             axis.ticks = element_blank(),
-             axis.text = element_blank(),
-             axis.title = element_blank(),
-             legend.position = c(0.5, 0.875),
-             legend.title = element_text(size = 13),
-             legend.text = element_text(family = "Montserrat"),
-             plot.title = element_text(size = 30,
-                                       face = "bold",
-                                       hjust = 0.5,
-                                       margin = margin(t = 10, b = 0)),
-             plot.subtitle = element_text(size = 18,
-                                          color = "grey55",
-                                          face = "bold",
-                                          hjust = 0.5,
-                                          margin = margin(t = 0, b = 15)),
-             plot.caption = element_text(size = 12,
-                                         color = "grey55",
-                                         hjust = 0.5,
-                                         margin = margin(t = 15, b = 10)))
+chain_investment %>% 
+  count(category)
 
 
-df_nps <-
-  readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-17/national_parks.csv")
-readr::read_csv(here::here("data", "2019_38", "50_us_states_all_data.csv")
-                
+main <- chain_investment%>%
+  filter(group_num==1)%>%    # Filter thanks to "group_num" attribute                     
+  mutate(lab=case_when(      # New column with simpler names
+    category=="Total basic infrastructure"~"Basic",
+    category=="Total digital infrastructure"~"Digital",
+    category=="Total social infrastructure"~"Social"
+  ))
+
+
+ggplot(
+  data=main,
+  aes(x=year,y=gross_inv_chain,fill=lab)      # Choose newly created lab column for fill
+)+
+  geom_area()+
+  labs(
+    title = "Evolution of investment on US infrastructures",
+    x="Year",
+    y="Investment (millions of 2012 $)"
+  )+
+  theme_minimal()
+
+
+
+# Reorder factors
+main$lab<-factor(
+  main$lab,
+  c(
+    "Digital",
+    "Social",
+    "Basic"
+  ))
+
+ggplot(
+  data=main,
+  aes(x=year,y=gross_inv_chain,fill=lab)   
+)+
+  geom_area()+
+  labs(
+    title = "Evolution of investment on US infrastructures",
+    x="Year",
+    y="Investment (millions of 2012 $)"
+  )+
+  theme_minimal()
